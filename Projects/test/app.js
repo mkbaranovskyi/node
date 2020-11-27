b7e40d6b71240cb78ce665648828528a26359b20
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2020-11-28 01:23:09 +0200

    projects updated

--- Projects/test/app.js
+++ Projects/test/app.js
@@ -1,27 +1,85 @@
 const express = require('express')
-const app = express()
 const fs = require('fs')
 const path = require('path')
+const util = require('util')
+const stream = require('stream')
+const pipeline = util.promisify(stream.pipeline)
+const { Transform } = stream
 
-fs.mkdir(
-	path.join(__dirname, 'createdFolder/inner'),
-	{
-		recursive: true
+const app = express()
+const PORT = process.env.PORT || 5000
+const server = require('http').createServer(app)
+// const uploadFolder = path.join(__dirname, 'uploads')
+
+const multer = require('multer')
+const storage = multer.diskStorage({
+	destination(req, file, cb) {
+		cb(null, 'uploads')
 	},
-	(err) => {
-		if (err) {
-			console.error(err)
-		}
+	filename(req, file, cb) {
+		console.log('filename')
+		// leaving the original name will make the duplicated overwrite the old files
+		cb(null, file.originalname)
 	}
-)
-fs.writeFile(
-	path.join(__dirname, 'createdFolder', 'fileName.txt'),
-	'text',
-	(err) => {
-		if (err) {
-			console.error(err)
-		}
+})
+const upload = multer({
+	storage,
+	limits: {
+		fileSize: 1e10 // 10 GB
+	},
+	fileFilter(req, file, cb) {
+		return cb(null, true)
+	}
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('./public'))
+
+app.post(
+	'/upload',
+	/*upload.single('sendfile'),*/ (req, res, next) => {
+		let chunks = new Transform()
+		req.on('readable', () => {
+			let chunk
+			while ((chunk = req.read()) !== null) {
+				chunks.push(chunk)
+				console.log(chunk)
+			}
+		})
+		req.on('end', () => {
+			const fileName = path.join(__dirname, 'uploads', 'file.pdf')
+			fs.writeFile(fileName, chunks.read(), (err) => {
+				if (err) next(err)
+				console.log('done')
+			})
+		})
+		// const img = new Transform()
+		// req.on('data', (chunk) => {
+		// 	img.push(chunk)
+		// })
+		// req.on('end', () => {
+		// 	const filename = path.join(uploadFolder, 'img.jpg')
+		// 	fs.promises.writeFile(filename, img.read()).catch(console.error)
+		// })
+
+		// pipeline(req, fs.createWriteStream(uploadFolder + '/file'))
+		// req.on('data', (chunk) => {
+		// 	body.push(chunk)
+		// })
+
+		// req.on('end', () => {
+		// 	fs.writeFile(
+		// 		path.join(__dirname, 'uploads', 'file'),
+		// 		Buffer.concat(body),
+		// 		() => {
+		// 			console.log('The file is written')
+		// 		}
+		// 	)
+		// })
+		// console.log(req.headers['x-file-id'])
+		res.send('ok')
 	}
 )
 
-app.listen(5000)
+server.listen(PORT)
