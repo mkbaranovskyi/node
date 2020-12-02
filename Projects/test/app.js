046cab440a9ee384ea8614f77f20dd846b7a1f4e
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2020-12-02 19:17:46 +0200

    mostly done, to finish the catch error

--- Projects/test/app.js
+++ Projects/test/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
+const mime = require('mime')
 const stream = require('stream')
 const pipeline = util.promisify(stream.pipeline)
 const { Transform } = stream
@@ -34,52 +35,113 @@ const upload = multer({
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static('./public'))
+
+const fileUploads = new Map()
+
+app.get('/status', (req, res, next) => {
+	console.log(fileUploads)
+	res.send(String(fileUploads.get(req.headers['file-id']) || 0))
+})
 
 app.post(
 	'/upload',
-	/*upload.single('sendfile'),*/ (req, res, next) => {
-		let chunks = new Transform()
-		req.on('readable', () => {
-			let chunk
-			while ((chunk = req.read()) !== null) {
-				chunks.push(chunk)
-				console.log(chunk)
+	/*upload.single('sendfile'),*/ async (req, res, next) => {
+		try {
+			const fileId = req.headers['file-id']
+			const mimeType = mime.getExtension(req.headers['file-type'])
+			// Filename + mimeType
+			const fileName = path.join(__dirname, 'uploads', fileId) + `.${mimeType}`
+			const startByte = req.headers['start-byte']
+
+			// If this is not the 1st upload and the recorded starting bytes don't match - error
+			if (!startByte && +startByte !== fileUploads.get(fileId)) {
+				console.error("BYTES DON'T MATCH!")
+				console.log('startByte: ', startByte)
+				console.log('Map: ', fileUploads.get(fileId))
+				return res.status(400).send('Wrong start byte')
 			}
-		})
-		req.on('end', () => {
-			const fileName = path.join(__dirname, 'uploads', 'file.pdf')
-			fs.writeFile(fileName, chunks.read(), (err) => {
-				if (err) next(err)
-				console.log('done')
-			})
-		})
-		// const img = new Transform()
-		// req.on('data', (chunk) => {
-		// 	img.push(chunk)
-		// })
-		// req.on('end', () => {
-		// 	const filename = path.join(uploadFolder, 'img.jpg')
-		// 	fs.promises.writeFile(filename, img.read()).catch(console.error)
+
+			// let chunks = new Transform()
+			// let chunksLength = fileUploads.get(fileId) || 0
+
+			let flag = 'a'
+			if (!startByte) {
+				console.log('FLAG: w')
+				flag = 'w'
+			}
+
+			await pipeline(req, fs.createWriteStream(fileName, { flag }))
+
+			fileUploads.delete(fileId)
+			res.status(200).send('Upload finished')
+		} catch (err) {
+			console.error(err)
+			if (err.code === 'ERR_STREAM_PREMATURE_CLOSE') {
+				const fileSize = fs.statSync(fileName).size
+				console.log(fileSize)
+
+				fileUploads.set(fileId, fileSize)
+				console.log(fileUploads)
+				res.send(`${fileSize}`)
+			} else {
+				next(err)
+			}
+		}
+
+		// req.on('readable', readData)
+
+		// req.on('close', () => {
+		// 	console.log('close')
 		// })
 
-		// pipeline(req, fs.createWriteStream(uploadFolder + '/file'))
-		// req.on('data', (chunk) => {
-		// 	body.push(chunk)
+		// req.on('break', () => {
+		// 	console.log('break')
+
+		// 	fs.writeFile(fileName, chunks.read(), { flag }, (err) => {
+		// 		if (err) next(err)
+		// 		// Make a note about this file
+		// 		fileUploads.set(fileId, chunksLength)
+		// 		console.log(chunksLength)
+
+		// 		res.status(500).send('Uploaded interrupted')
+		// 		// res.destroy()
+		// 	})
 		// })
 
 		// req.on('end', () => {
-		// 	fs.writeFile(
-		// 		path.join(__dirname, 'uploads', 'file'),
-		// 		Buffer.concat(body),
-		// 		() => {
-		// 			console.log('The file is written')
-		// 		}
-		// 	)
+		// 	console.log('end')
+		// 	let flag = 'a'
+		// 	if (!startByte) {
+		// 		console.log('Flag: w')
+		// 		flag = 'w'
+		// 	}
+		// 	fs.writeFile(fileName, chunks.read(), { flag }, (err) => {
+		// 		if (err) next(err)
+
+		// 		// console.log('done')
+		// 		res.json({
+		// 			fileId: fileId,
+		// 			finished: true
+		// 		})
+		// 	})
 		// })
-		// console.log(req.headers['x-file-id'])
-		res.send('ok')
+
+		// function readData() {
+		// 	let chunk
+		// 	while ((chunk = req.read()) !== null) {
+		// 		chunks.push(chunk)
+		// 		chunksLength += chunk.length
+
+		// 		// if (chunksLength >= 3e6) {
+		// 		// 	req.removeListener('readable', readData)
+		// 		// 	req.emit('break')
+		// 		// 	break
+		// 		// }
+		// 	}
+		// }
 	}
 )
+
+app.use(express.static('./public'))
 
 server.listen(PORT)
