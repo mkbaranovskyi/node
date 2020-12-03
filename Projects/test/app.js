10d3ea14c6c478797ade19b9f9f6ac68472508dc
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2020-12-03 04:11:23 +0200

    Resumable upload done, to finish progress bar

--- Projects/test/app.js
+++ Projects/test/app.js
@@ -5,12 +5,10 @@ const util = require('util')
 const mime = require('mime')
 const stream = require('stream')
 const pipeline = util.promisify(stream.pipeline)
-const { Transform } = stream
 
 const app = express()
 const PORT = process.env.PORT || 5000
 const server = require('http').createServer(app)
-// const uploadFolder = path.join(__dirname, 'uploads')
 
 const multer = require('multer')
 const storage = multer.diskStorage({
@@ -47,98 +45,35 @@ app.post(
 	'/upload',
 	/*upload.single('sendfile'),*/ async (req, res, next) => {
 		try {
-			const fileId = req.headers['file-id']
+			global.fileId = req.headers['file-id']
 			const mimeType = mime.getExtension(req.headers['file-type'])
 			// Filename + mimeType
-			const fileName = path.join(__dirname, 'uploads', fileId) + `.${mimeType}`
+			global.fileName =
+				path.join(__dirname, 'uploads', global.fileId) + `.${mimeType}`
 			const startByte = req.headers['start-byte']
 
 			// If this is not the 1st upload and the recorded starting bytes don't match - error
-			if (!startByte && +startByte !== fileUploads.get(fileId)) {
+			if (!startByte && +startByte !== fileUploads.get(global.fileId)) {
 				console.error("BYTES DON'T MATCH!")
 				console.log('startByte: ', startByte)
-				console.log('Map: ', fileUploads.get(fileId))
+				console.log('Map: ', fileUploads.get(global.fileId))
 				return res.status(400).send('Wrong start byte')
 			}
 
-			// let chunks = new Transform()
-			// let chunksLength = fileUploads.get(fileId) || 0
+			await pipeline(req, fs.createWriteStream(global.fileName, { flags: 'a' }))
 
-			let flag = 'a'
-			if (!startByte) {
-				console.log('FLAG: w')
-				flag = 'w'
-			}
-
-			await pipeline(req, fs.createWriteStream(fileName, { flag }))
-
-			fileUploads.delete(fileId)
 			res.status(200).send('Upload finished')
 		} catch (err) {
 			console.error(err)
 			if (err.code === 'ERR_STREAM_PREMATURE_CLOSE') {
-				const fileSize = fs.statSync(fileName).size
-				console.log(fileSize)
-
-				fileUploads.set(fileId, fileSize)
-				console.log(fileUploads)
 				res.send(`${fileSize}`)
 			} else {
 				next(err)
 			}
+		} finally {
+			// Remember the uploaded data size
+			fileUploads.set(global.fileId, fs.statSync(global.fileName).size)
 		}
-
-		// req.on('readable', readData)
-
-		// req.on('close', () => {
-		// 	console.log('close')
-		// })
-
-		// req.on('break', () => {
-		// 	console.log('break')
-
-		// 	fs.writeFile(fileName, chunks.read(), { flag }, (err) => {
-		// 		if (err) next(err)
-		// 		// Make a note about this file
-		// 		fileUploads.set(fileId, chunksLength)
-		// 		console.log(chunksLength)
-
-		// 		res.status(500).send('Uploaded interrupted')
-		// 		// res.destroy()
-		// 	})
-		// })
-
-		// req.on('end', () => {
-		// 	console.log('end')
-		// 	let flag = 'a'
-		// 	if (!startByte) {
-		// 		console.log('Flag: w')
-		// 		flag = 'w'
-		// 	}
-		// 	fs.writeFile(fileName, chunks.read(), { flag }, (err) => {
-		// 		if (err) next(err)
-
-		// 		// console.log('done')
-		// 		res.json({
-		// 			fileId: fileId,
-		// 			finished: true
-		// 		})
-		// 	})
-		// })
-
-		// function readData() {
-		// 	let chunk
-		// 	while ((chunk = req.read()) !== null) {
-		// 		chunks.push(chunk)
-		// 		chunksLength += chunk.length
-
-		// 		// if (chunksLength >= 3e6) {
-		// 		// 	req.removeListener('readable', readData)
-		// 		// 	req.emit('break')
-		// 		// 	break
-		// 		// }
-		// 	}
-		// }
 	}
 )
 
