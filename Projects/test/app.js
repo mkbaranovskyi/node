502d7613fe6aadf7835603b5a9eee5d68e19db01
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2020-12-15 23:40:59 +0200

    streams rework started

--- Projects/test/app.js
+++ Projects/test/app.js
@@ -1,26 +1,17 @@
+const util = require('util')
 const fs = require('fs')
-const path = require('path')
-const events = require('events')
+const zlib = require('zlib')
 
-const r = fs.createReadStream(
-	path.join(__dirname, 'uploads', "01. Lustra - Scotty doesn't know.mp3")
-)
-const w = fs.createWriteStream(path.join(__dirname, 'uploads', 'output'))
+const pipeline = util.promisify(require('stream').pipeline)
 
-run(r, w)
-
-async function run(origin, dest) {
-	try {
-		for await (const chunk of origin) {
-			isDrained = dest.write(chunk)
-			// if the writing buffer is full - await until it's empty
-			if (!isDrained) {
-				await events.once(dest, 'drain')
-			}
-		}
-	} catch (err) {
-		origin.destroy()
-		dest.destroy()
-		console.error(err)
-	}
+async function run() {
+	await pipeline(
+		fs.createReadStream('archive.tar'),
+		zlib.createGzip(),
+		fs.createWriteStream('archive.tar.gz')
+	)
+	throw new Error('oops')
+	console.log('Pipeline succeeded.')
 }
+
+run().catch((err) => console.error('yo'), '!!!')
