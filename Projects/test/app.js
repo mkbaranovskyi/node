be37d51e1a1d25e55ab911e2c91e16ad9e04e761
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2020-12-25 23:37:45 +0200

    sql attributes

--- Projects/test/app.js
+++ Projects/test/app.js
@@ -1,11 +1,82 @@
 const express = require('express')
 const app = express()
-const Cookies = require('js-cookie')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res, next) => {
-	res.end('<b>Hello</b>')
+const { check, matchedData, validationResult } = require('express-validator')
+
+const multer = require('multer')
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		console.log(file) // <--- Uploaded file info
+		cb(null, path.join(__dirname, 'uploads'))
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname)
+	}
 })
+const upload = multer({ storage })
 
-app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.listen(5000)
+app.post(
+	'/form',
+	upload.single('send-file'),
+	// The Gate: our validators and sanitizers
+	[
+		check('username') // <input name="username">
+			.isLength({ min: 3 })
+			.withMessage('Your name should be at least 3 characters long') // Return this error msg if the previous check failed
+			.bail() // Stop here if the previous checks faileD
+			.trim(), // Trim spaces
+		check('e-mail') // <input name="e-mail">
+			.isEmail()
+			.withMessage('Incorrect email')
+			.bail()
+			.trim()
+			.normalizeEmail(), // toLowerCase() and similar normalization stuff
+		check('gender') // <input name="gender">
+			.custom((value) => {
+				// Our custom check for radio buttons
+				switch (value) {
+					case 'male':
+						return true
+					case 'female':
+						return true
+					case 'other':
+						return true
+					default:
+						return false
+				}
+			})
+			.withMessage('Please select your gender')
+	],
+	(req, res, next) => {
+		try {
+			// We appoint it to `req` to make accessible in `catch`
+			const result = (req.result = validationResult(req))
+			const matches = matchedData(req)
+
+			// Results (including errors if any)
+			// console.log(result)
+			// // These values successfully passed all their checks
+			// console.log('Matched Data: ', matches)
+
+			// Simple way to learn if there were any errors and jump to the `catch` block. You can also check `result.errors` manually for detailed inspection
+			result.throw()
+			console.log('yo')
+			// If we came here - there were no errors
+			res.json({ msg: "You're successfully subscribed!" })
+		} catch (err) {
+			console.error('Error of validation!')
+			// Send errors back to the client so they know what went wrong
+			res.json({ errors: req.result.errors })
+			next(err)
+		}
+	}
+)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT)
