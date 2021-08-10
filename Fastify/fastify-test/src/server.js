ace27318805a9c90410b47d0a7e6508f3fee7301
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2021-08-10 08:52:03 +0300

    Fastify test

---
+++ Fastify/fastify-test/src/server.js
@@ -0,0 +1,23 @@
+const fastify = require('fastify')({ logger: true })
+// Plugins should be registered
+fastify.register(require('fastify-swagger'), {
+  exposeRoute: true,
+  routePrefix: '/docs',
+  swagger: {
+    info: { title: 'fasfity-api' }
+  }
+})
+fastify.register(require('./routes/items'))
+
+const PORT = process.env.PORT || 5000
+
+const start = async () => {
+  try{
+    await fastify.listen(PORT)
+  } catch (err){
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
