fd14b97436827e49b3d5dd7b51f4ce7e77c15ff8
Maksym Baranovskyi <maksym.b@psoft.pro>
2022-10-20 15:07:20 +0300

    fundamentals update

--- Nest/fundamentals/src/shared/database/migrations/1663256433783-coffee-added-description.ts
+++ Nest/fundamentals/src/shared/database/migrations/1663256433783-coffee-added-description.ts
@@ -1,14 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class coffeeAddedDescription1663256433783 implements MigrationInterface {
-    name = 'coffeeAddedDescription1663256433783'
+  name = 'coffeeAddedDescription1663256433783'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "coffee" DROP COLUMN "description"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "coffee" DROP COLUMN "description"`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "coffee" ADD "description" character varying`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "coffee" ADD "description" character varying`);
+  }
 }
